cf990b92f56f49bb0fd265997ebebd0130bb1d0a
CRBroughton <crbroughton@posteo.uk>
2024-10-07 08:50:58 +0100

    feat(components): :sparkles: create basic Row component

---
+++ app/common/components/Row/Row.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { h } from 'vue'
+import Card from '~/common/patterns/Card/Card.vue'
+import Image from '../Image/Image.vue'
+import Row from './Row.vue'
+
+const meta = {
+  title: 'Components/Row',
+  component: Row,
+} satisfies Meta<typeof Row>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+function createSkeletons() {
+  const skeletons: VNode[] = []
+  for (let index = 0; index < 10; index++) {
+    skeletons.push(
+      h(Card, null, {
+        default: () => h(
+          Image,
+          { src: 'https://via.placeholder.com/300', alt: 'test' },
+        ),
+        heading: () => 'Product Name',
+        subheading: () => 'Short description of the product goes here. It should be engaging and informative.',
+        amount: () => '$29.99',
+      }),
+    )
+  }
+  return skeletons
+}
+export const Primary: Story = {
+  render(args) {
+    return h(Row, {
+      ...args,
+      gap: 2,
+      wrap: true,
+    }, {
+      default: () => createSkeletons(),
+    })
+  },
+}
---
+++ app/common/components/Row/Row.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    type?: string
+    gap?: RowProps['gap']
+    wrap?: RowProps['wrap']
+    reverse?: RowProps['reverse']
+  }>(),
+  {
+    type: 'div',
+    gap: 2,
+    wrap: false,
+    reverse: false,
+  },
+)
+
+type RowProps = CVAProps<typeof row>
+const row = cva('flex', {
+  variants: {
+    gap: {
+      2: 'gap-2',
+      4: 'gap-4',
+      8: 'gap-8',
+    },
+    wrap: {
+      true: 'flex-wrap',
+    },
+    reverse: {
+      true: 'flex-wrap-reverse',
+    },
+  },
+})
+</script>
+
+<template>
+  <Component :is="props.type" :class="row({ gap, wrap })">
+    <slot />
+  </Component>
+</template>
